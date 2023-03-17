92c49f73d7766b714ffef8c58eadc7d4067b8665
1lucas1apk <dowfriends@gmail.com>
2023-03-16 20:21:12 -0400

    Add files via upload
    Signed-off-by: Lucas Morais Rodrigues <76886832+1Lucas1apk@users.noreply.github.com>

--- @Rest/MoonlinkDatabase.js
+++ @Rest/MoonlinkDatabase.js
@@ -2,10 +2,8 @@
 const fs = require('fs')
 const path = require('path')
 class MoonlinkDB {
-  data;
-
+  data = {};
   constructor() {
-    this.fetch()
   }
   set(key, value) {
 
@@ -18,7 +16,6 @@ class MoonlinkDB {
         data[key_s[0]] = { ...data[key_s[0]] };
         obj(data[key_s[0]], key_s.slice(1))
       }
-      this.#save()
     }
     obj(this.data, keys)
 
@@ -33,10 +30,7 @@ class MoonlinkDB {
     const oldArray = this.get(key) ?? [];
     if (!(Array.isArray(oldArray)) && oldArray !== undefined) throw new TypeError('The value of the provided key must be an array');
     oldArray.push(value);
-
     this.set(key, oldArray);
-
-    this.#save();
     return this.get(key.split('.')[0]);
   }
 
@@ -46,27 +40,6 @@ class MoonlinkDB {
       if (i === arr.length - 1) delete o?.[curr];
       else return o?.[curr];
     }, this.data);
-    this.#save()
   }
-  fetch() {
-    try {
-      let dateTemp = JSON.parse(fs.readFileSync(__dirname + '/database.json'), 'utf8')
-
-      if (!dateTemp) this.data = {};
-      this.data = dateTemp
-    } catch (err) {
-      console.log(err)
-      if (err.code == 'ENOENT') this.data = {};
-      console.log('[ く⁠コ⁠:⁠彡 ]: data not found, creating a new database');
-    }
-  }
-  #save() {
-    try {
-      fs.writeFileSync(__dirname + '/database.json', JSON.stringify(this.data, null, 2));
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
 }
 module.exports = MoonlinkDB;
