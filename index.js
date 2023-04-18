59014c41a8e08259b640637a1dbcbd58c877f9c4
1lucas1apk <dowfriends@gmail.com>
2023-04-17 21:14:34 -0400

    Delete index.js
    Signed-off-by: Lucas Morais Rodrigues <76886832+1Lucas1apk@users.noreply.github.com>

--- index.js
+++
@@ -1,7 +0,0 @@
-const { MoonlinkManager } = require('./@Moonlink/MoonlinkManager.js')
-const version = require('./package.json').version
-
-module.exports = { 
-    MoonlinkManager, 
-    version
-}
