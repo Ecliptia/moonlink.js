73fe9d9ed5c2f1569b01c557ca015f51bcd93f35
1lucas1apk <dowfriends@gmail.com>
2023-08-26 16:42:41 +0000

    Forgot to change the version

--- dist/index.js
+++ dist/index.js
@@ -15,7 +15,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.version = void 0;
-exports.version = "2.5.12";
+exports.version = "2.5.24";
 __exportStar(require("./src/@Moonlink/MoonlinkManager"), exports);
 __exportStar(require("./src/@Moonlink/MoonlinkPlayers"), exports);
 __exportStar(require("./src/@Moonlink/MoonlinkNodes"), exports);
