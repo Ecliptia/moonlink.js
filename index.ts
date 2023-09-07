e0cf46983395dd5afe2f1d21fe1b0696dccfa2ee
1lucas1apk <dowfriends@gmail.com>
2023-09-07 19:18:00 +0000

    . types

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export const version: string = "2.10.94";
+export const version: string = "2.10.95";
 export * from "./src/@Moonlink/MoonlinkManager";
 export * from "./src/@Moonlink/MoonlinkWebsocket";
 export * from "./src/@Moonlink/MoonlinkPlayers";
