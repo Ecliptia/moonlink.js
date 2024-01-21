fc06c024d44e0dd9d03d3d08a581e712fcacd836
Lucas Morais Rodrigues <76886832+1Lucas1apk@users.noreply.github.com>
2024-01-21 11:17:38 -0400

    Update tokens.config.ts
    Signed-off-by: Lucas Morais Rodrigues <76886832+1Lucas1apk@users.noreply.github.com>

--- docs/tokens.config.ts
+++ docs/tokens.config.ts
@@ -1,18 +1,3 @@
 import { defineTheme } from 'pinceau'
 
-export default defineTheme({
-  color: {
-    primary: {
-      50: "#E5F2FF",
-      100: "#CCD5FF",
-      200: "#99AAFF",
-      300: "#6675FF",
-      400: "#3D33FF",
-      500: "#0004FF",
-      600: "#0036CC",
-      700: "#005499",
-      800: "#003566",
-      900: "#001833"
-    }
-  }
-})
+export default defineTheme({})
