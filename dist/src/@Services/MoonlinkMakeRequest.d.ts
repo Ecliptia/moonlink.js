a74ab9e1925b298f444c10aad618cda46bb8edb8
1Lucas1apk <dowfriends@gmail.com>
2024-03-15 21:08:19 -0400

    clean

--- dist/src/@Services/MoonlinkMakeRequest.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-/// <reference types="node" />
-import * as http from "http";
-import * as https from "https";
-export declare function makeRequest<T>(uri: string, options: http.RequestOptions | (https.RequestOptions & {
-    method?: string;
-}), data?: Record<string, any>): Promise<T>;
