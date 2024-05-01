43b0214259d2ac073c940134566bed0744558508
Lucas Morais Rodrigues <76886832+1Lucas1apk@users.noreply.github.com>
2024-05-01 14:04:40 -0400

    change

--- testBot/index.js
+++ testBot/index.js
@@ -43,49 +43,13 @@ for (const folder of commandFolders) {
 
 client.moon = new MoonlinkManager(
   [{
-    host: "541b2cbb-4e46-4064-b8de-53bbe5ac63dc-00-20h37tgfzow4w.kirk.replit.dev",
-    password: "maybeiwasboring", secure: true, port: 443
-  }, /* {
-    host: "lavalink.jirayu.pw",
-    port: 2333,
-    password: "youshallnotpass",
-    secure: false
-  }*/ {
     host: "localhost",
     port: 2333,
     secure: false,
     identifier: "NODELINK"
-  } /*{
-    host: "65.108.101.89",
-    port: 17958,
-    secure: false,
-    identifier: "LAVALINK-2"
-  },
-    {
-      identifier: "It'z Zoldy 2",
-      host: "139.99.124.43",
-      port: 7784,
-      password: "PasswordIsZoldy",
-      secure: false,
-    },{
-      host: "lavalink-v4.teramont.net",
-      port: 443,
-      password: "eHKuFcz67k4lBS64",
-      secure: true
-    }/*{
-    host: "localhost", port: 2333, secure: false, password: require("../config.json").password
-  }*/ /*{
-    host: "fbfcdd0d-e644-4b03-a632-1237cb5b6077-00-1ecqthk8a8ux8.kirk.replit.dev", secure: true, port: 443, identifier: "NODELINK", isNodeLink: true
-  },*//*{
-    host: "185.174.136.205",
-    password: "123123123",
-    port: 2333,
-    secure: false
-  }*/],
+  }],
   {
     //http2: true,
-    resume: true,
-    autoResume: true,
     clientName: "Moonlink/Blio"
   },
   (id, data) => {
@@ -128,4 +92,4 @@ client.on("error", (error) => {
   log(`[ Client ]: Client error: ${error}`);
 });
 
-client.login(require ("../config.json").token);
+client.login(require("../config.json").token);
