41a348704ee0089f38c150708ed1047d7103b6bb
Lucas Morais Rodrigues <76886832+1Lucas1apk@users.noreply.github.com>
2024-05-11 10:47:31 -0400

    Change: Switched from HTTP to HTTPS for requests, utilizing Node.js's global fetch function.

--- dist/src/@Services/MoonlinkMakeRequest.d.ts
+++ dist/src/@Services/MoonlinkMakeRequest.d.ts
@@ -1,7 +1,2 @@
 /// <reference types="node" />
-/// <reference types="node" />
-import * as http from "http";
-import * as https from "https";
-export declare function makeRequest<T>(uri: string, options: http.RequestOptions | (https.RequestOptions & {
-    method?: string;
-}), data?: Record<string, any>): Promise<T>;
+export declare function makeRequest<T>(url: string, options: RequestInit, data?: any): Promise<T>;
