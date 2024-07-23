ffd304a27e26f526b9a4b94054f163d9140619bc
Lucas Morais Rodrigues <76886832+1Lucas1apk@users.noreply.github.com>
2024-07-23 15:10:02 -0400

    Delete dist directory
    Signed-off-by: Lucas Morais Rodrigues <76886832+1Lucas1apk@users.noreply.github.com>

--- dist/src/typings/types.d.ts
+++
@@ -1,6 +0,0 @@
-export type TSearchSources = "youtube" | "youtubemusic" | "soundcloud";
-export type TLoadResultType = "track" | "playlist" | "search" | "empty" | "error" | TLoadResultNodeLinkType;
-export type TLoadResultNodeLinkType = "short" | "album" | "artist" | "playlist" | "station" | "podcast" | "show";
-export type TSortTypeNode = "players" | "playingPlayers" | "memory" | "cpuLavalink" | "cpuSystem" | "uptime" | "random";
-export type TPlayerLoop = "off" | "track" | "queue";
-export type TTrackEndType = "queueEnd" | "loadFailed" | "stopped" | "replaced" | "cleanup" | "finish";
