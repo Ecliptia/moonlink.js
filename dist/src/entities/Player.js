a9b611bd42e277ee897f22005ff2ce60c6f35e5b
1lucas1apk <dowfriends@gmail.com>
2024-07-25 11:23:12 -0400

    fixed: player destruction

--- dist/src/entities/Player.js
+++ dist/src/entities/Player.js
@@ -36,7 +36,7 @@ class Player {
         this.paused = false;
         this.queue = new index_1.Queue();
         this.node = this.manager.nodes.get(config.node);
-        if (manager.options.NodeLinkFeatures) {
+        if (manager.options.NodeLinkFeatures || this.node.info.isNodeLink) {
             this.listen = new index_1.Listen(this);
             this.lyrics = new index_1.Lyrics(this);
         }
