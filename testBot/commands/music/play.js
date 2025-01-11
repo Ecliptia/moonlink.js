3124e1284732cb49d0b45fe8a4070bacf30edd95
1lucas1apk <dowfriends@gmail.com>
2025-01-11 13:37:15 -0400

    chore: improve player state handling and ensure user data is correctly assigned

--- testBot/commands/music/play.js
+++ testBot/commands/music/play.js
@@ -1,26 +1,28 @@
 module.exports = {
-    data: {
-      name: "play",
-    },
-    async execute(client, message, args) {
-      const channel = message.member.voice.channel;
-      if (!channel) return message.reply("You need to join a voice channel first!");
-      if (!args[0]) return message.reply("You need to provide a song or URL!");
-  
-      let player = client.manager.players.get(message.guild.id) || client.manager.createPlayer({
+  data: {
+    name: "play",
+  },
+  async execute(client, message, args) {
+    const channel = message.member.voice.channel;
+    if (!channel) return message.reply("You need to join a voice channel first!");
+    if (!args[0]) return message.reply("You need to provide a song or URL!");
+
+    let player =
+      client.manager.players.get(message.guild.id) ||
+      client.manager.createPlayer({
         guildId: message.guild.id,
         voiceChannelId: channel.id,
         textChannelId: message.channel.id,
+        autoPlay: true,
       });
-  
-      if (!player.connected) player.connect({ setDeaf: true });
-  
-      const searchResult = await client.manager.search({ query: args.join(" ") });
-      if (!searchResult.tracks.length) return message.reply("No results found.");
-  
-      player.queue.add(searchResult.tracks[0], message.author.id);
-      if (!player.playing) player.play();
-      await message.reply(`Playing track: ${searchResult.tracks[0].title}`);
-    },
-  };
-  
+
+    if (!player.connected) player.connect({ setDeaf: true });
+
+    const searchResult = await client.manager.search({ query: args.join(" ") });
+    if (!searchResult.tracks.length) return message.reply("No results found.");
+
+    player.queue.add(searchResult.tracks[0], message.author.id);
+    if (!player.playing) player.play();
+    await message.reply(`Playing track: ${searchResult.tracks[0].title}`);
+  },
+};
