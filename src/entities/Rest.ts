97c2753334620bdf9be1b34f56bc910c30549c22
1lucas1apk <dowfriends@gmail.com>
2025-03-23 15:13:18 -0400

    chore: update User-Agent string and version in headers; add changelog and update version in package.json

--- src/entities/Rest.ts
+++ src/entities/Rest.ts
@@ -15,7 +15,7 @@ export class Rest {
     this.defaultHeaders = {
       Authorization: this.node.password,
       Accept: "application/json",
-      "User-Agent": `Moonlink.js/${node.manager.version} (PETRICHOR/14.02.24)`,
+      "User-Agent": `Moonlink.js/${node.manager.version} (AFTERGLOW/23.03.25)`,
       "Content-Type": "application/json",
       "accept-encoding": "br, gzip, deflate"
     };
