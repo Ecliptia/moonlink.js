20f7fbaa224282d1c7feb5ac08487a5986981b7e
1Lucas1apk <dowfriends@gmail.com>
2025-04-26 11:43:35 -0400

    fix: when bot disconnected and played again, it played 3s from the beginning and went back
    Modified the player position assignment to set it to 0 only when the loop is not "off". This change ensures that the position is undefined when looping is disabled, improving the handling of playback scenarios.

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -210,7 +210,7 @@ export class Player {
           encoded: this.current.encoded,
           userData: options.requestedBy ?? this.current?.requestedBy ?? undefined,
         },
-        position: options.position ?? 0,
+        position: options.position ?? (this.loop !== "off" ? 0 : undefined),
         endTime: options.endTime ?? undefined,
         volume: this.volume,
       },
