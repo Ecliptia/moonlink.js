bf67462460d067ce89a41fc6d9b38d8e9afae860
1Lucas1apk <dowfriends@gmail.com>
2025-04-26 16:11:50 -0400

    fix: Update User-Agent in defaultHeaders to reflect new version and branding

--- dist/src/entities/Rest.js
+++ dist/src/entities/Rest.js
@@ -12,7 +12,7 @@ class Rest {
         this.defaultHeaders = {
             Authorization: this.node.password,
             Accept: "application/json",
-            "User-Agent": `Moonlink.js/${node.manager.version} (AFTERGLOW/23.03.25)`,
+            "User-Agent": `Moonlink.js/${node.manager.version} (Entropy/26.04.25)`,
             "Content-Type": "application/json",
             "accept-encoding": "br, gzip, deflate"
         };
