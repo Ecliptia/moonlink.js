ab69baad289b4dee8a06d388d0d18abbb8db267a
1lucas1apk <dowfriends@gmail.com>
2025-06-10 12:24:59 -0400

    fix: update typings, SourceManager, and bot settings; improve error handling and add debug logging
    - Adjusted `IEvents` interface by modifying the `nodeRaw` event arguments.
    - Added `disableNativeSources` check and debug logging in `SourceManager.ts` constructor.
    - Enhanced error handling for Spotify token fetching with detailed warnings.
    - Refined `play.js` to support local sources and improve artwork color extraction.
    - Updated bot settings with new Lavalink server configuration and `disableNativeSources` property.
    - Incremented version to `4.6.18` in `package.json`.
    - Emitted `nodeRaw` event in `Node.js` for enhanced data tracking.

--- src/management/SourceManager.ts
+++ src/management/SourceManager.ts
@@ -5,6 +5,13 @@ export class SourceManager {
   constructor(manager: Manager) {
     this.manager = manager;
     this.sources = {};
+    if (!this.manager.options.disableNativeSources) {
+      this.manager.emit("debug", "Moonlink.js > Sources are enabled");
+    } else {
+      this.manager.emit("debug", "Moonlink.js > Sources are disabled");
+      return;
+    }
+
     this.loadFolder();
   }
     public add(source: ISource): void {
