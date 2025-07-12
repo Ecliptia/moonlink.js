4c752467256b0df3e02b3cdfc588cd05471f33a7
1lucas1apk <dowfriends@gmail.com>
2025-07-12 00:22:23 -0400

    fix: update bot node configuration
    **Details:**
    - Changed node port from `3000` to `2333`.
    - Updated node password for secure connection.
    - Ensured configuration supports multiple regions.

--- testBot/bot.js
+++ testBot/bot.js
@@ -11,8 +11,8 @@ client.manager = new Manager({
   nodes: [
     {
       host: "localhost",
-      port: 3000,
-      password: "123",
+      port: 2333,
+      password: "yeah",
       region: ["us", "eu", "singapore", "sydney", "brazil", "hongkong", "russia"],
       identifier: "MAIN",
       secure: false,
