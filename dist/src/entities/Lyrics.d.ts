9a71f3fd6c92a83ba34151d2a561287360bc628d
1lucas1apk <dowfriends@gmail.com>
2025-07-12 00:28:38 -0400

    **refactor: remove deprecated Database class**
    **Details:**
    - Deleted `Database` class and associated TypeScript definitions.
    - Removed implementation of WAL, snapshot, and database management logic.
    - Cleaned up related references to filesystem and database operations.
    - Improved codebase by removing unused and redundant components.

--- dist/src/entities/Lyrics.d.ts
+++
@@ -1,7 +0,0 @@
-import { Player } from './Player';
-import { IRESTGetLyrics } from '../typings/Interfaces';
-export declare class Lyrics {
-    player: Player;
-    constructor(player: any);
-    getLyrics(): Promise<IRESTGetLyrics>;
-}
