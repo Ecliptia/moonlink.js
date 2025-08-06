1ec52eb21c339a076400675207b941990481fc6f
1lucas1apk <dowfriends@gmail.com>
2025-08-06 11:06:59 -0400

    chore: bump package version to 4.60.4 and update User-Agent date
    **Details:**
    - Updated `package.json` version from `4.52.2` to `4.60.4`.
    - Adjusted `User-Agent` date to `06.08.2025` for consistency.
    - Synced changes across TypeScript and JavaScript distribution files.

--- dist/src/entities/Rest.js
+++ dist/src/entities/Rest.js
@@ -12,7 +12,7 @@ class Rest {
         this.defaultHeaders = {
             Authorization: this.node.password,
             Accept: "application/json",
-            "User-Agent": `Moonlink.js/${node.manager.version} (Epiphany/16.07.2025)`,
+            "User-Agent": `Moonlink.js/${node.manager.version} (Epiphany/06.08.2025)`,
             "Content-Type": "application/json",
             "accept-encoding": "br, gzip, deflate"
         };
