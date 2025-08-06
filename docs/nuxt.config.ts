802c835af67e93cf154c1b49489245e8e5d865de
1lucas1apk <dowfriends@gmail.com>
2025-08-06 14:29:42 -0400

    chore(docs): update nuxt.config formatting and remove unused SSR/Nitro config

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,14 +1,16 @@
-// nuxt.config.ts
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
   i18n: {
     defaultLocale: 'en',
-    locales: [{ code: 'en', name: 'English', language: 'en-US' }],
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        language: 'en-US',
+      },
+    ],
   },
   compatibilityDate: '2024-07-06',
-  nitro: {
-    preset: 'netlify'
-  }
-})
+});
