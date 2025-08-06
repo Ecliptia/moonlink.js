d333797d98e850603c64eea61b2e1b42cb1c663c
1lucas1apk <dowfriends@gmail.com>
2025-08-06 14:10:57 -0400

    chore(docs): downgrade Nuxt to v3.17.6 and disable SSR

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,16 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
   i18n: {
     defaultLocale: 'en',
-    locales: [
-      {
-        code: 'en',
-        name: 'English',
-        language: 'en-US',
-      },
-    ],
+    locales: [{ code: 'en', name: 'English', language: 'en-US' }],
   },
   compatibilityDate: '2024-07-06',
-});
+})
