2ed6951f4aacd7ce1d8ba5e31547cd917eafea4a
1lucas1apk <dowfriends@gmail.com>
2025-11-15 13:52:30 -0400

    Delete dist directory
    Signed-off-by: Lucas Morais Rodrigues <76886832+1Lucas1apk@users.noreply.github.com>

--- dist/src/connectors/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Connectors = void 0;
-const DiscordJs_1 = require("./DiscordJs");
-exports.Connectors = {
-    DiscordJs: DiscordJs_1.DiscordJs,
-};
-//# sourceMappingURL=index.js.map
