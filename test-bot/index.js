2cfc42370749342c8a126431f41434b2ba5af3ab
1lucas1apk <dowfriends@gmail.com>
2025-11-18 12:24:37 -0400

    Refactor session management in Node class; disable resume option in test-bot configuration
    - Removed session resume logic from Node class, including session retrieval and storage.
    - Updated WebSocket connection headers to exclude session ID.
    - Adjusted player restoration logic to simplify state management.
    - Disabled session resume feature in test-bot configuration for testing purposes.

--- test-bot/index.js
+++ test-bot/index.js
@@ -40,7 +40,7 @@ const manager = new Manager({
             path: "./database"
         },
         autoResume: true,
-        resume: true, 
+        resume: false, 
         customFilters: {
             "Reset": {},
             "Alien": "pitch=1.5,vibrato=f=3:d=0.7",
