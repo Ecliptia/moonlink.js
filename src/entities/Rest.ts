4a0ccfc4ef11c9d40e416c0d0d8c7525f3352ba5
1lucas1apk <dowfriends@gmail.com>
2025-12-07 14:02:40 -0400

    fix: update User-Agent version in headers to reflect the latest release date

--- src/entities/Rest.ts
+++ src/entities/Rest.ts
@@ -15,7 +15,7 @@ export class Rest {
     this.defaultHeaders = {
       Authorization: this.node.password,
       Accept: "application/json",
-      "User-Agent": `Moonlink.js/${node.manager.version} (Malodorus/06.09.2025)`,
+      "User-Agent": `Moonlink.js/${node.manager.version} (Malodorus/07.11.2025)`,
       "Content-Type": "application/json",
       "accept-encoding": "br, gzip, deflate"
     };
